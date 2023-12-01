859d3368e9caaae8d91ae8f2b1e4876a5ea3b5ba
Matteo Orlando <mat.orlando88@gmail.com>
2023-12-01 17:13:00 +0100

    first commit

---
+++ PROGETTO2 JAVA/SriptJava.js
@@ -0,0 +1,4 @@
+console.log('Hello World')
+// Il Mestiere del programmatore si incentra sulla rislozione di problemi
+// ALgoritmo! un ALGORITMO é una serie di passaggi, ORDINATI, che ci porta alla soluzione di un problema
+// una serie di istruzioni si defiscono ALGORITMI se, lo stesso identico problema, porta SEMPRE alla stessa soluzione.
